e5a937fb73e0212ef034746d62757b1eed23dfb0
Matias Rubiolo <rubiolomatias2001@gmail.com>
2023-10-07 14:15:32 -0300

    feat:added visitor entity

--- server/src/http/routes/common.routes.ts
+++ server/src/http/routes/common.routes.ts
@@ -1,19 +1,18 @@
-import { Application } from 'express';
+import { Application } from 'express'
 
 export default abstract class CommonRoutes {
-  protected app: Application;
-  private name: string;
+  protected app: Application
+  private name: string
 
-  constructor(app: Application, name: string) {
-    this.app = app;
-    this.name = name;
-    this.setUpRoutes();
+  constructor (app: Application, name: string) {
+    this.app = app
+    this.name = name
+    this.setUpRoutes()
   }
 
-  public getName() {
-    return this.name;
+  public getName () {
+    return this.name
   }
 
   abstract setUpRoutes(): Application;
 }
-
