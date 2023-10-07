6540c1cbe5c6d6737abafd99ebd0fb6607b5c612
Matias Rubiolo <rubiolomatias2001@gmail.com>
2023-10-07 18:26:48 -0300

    feat:added claim entity

---
+++ server/src/domain/entities/claim.entity.ts
@@ -0,0 +1,41 @@
+import { v4 } from 'uuid'
+import Visitor from './visitor.entity'
+import Category from './category.entity'
+
+class Claim {
+  private id: string
+  private owner: Visitor
+  private title: string
+  private description: string
+  private category: Category
+  private location: string
+  private createdAt: Date
+  private cloneOf: null | Claim
+
+  private constructor (id:string, owner: Visitor, title: string,
+    description: string, category: Category, location: string, createdAt: Date,
+    cloneOf: Claim) {
+    this.id = id
+    this.owner = owner
+    this.title = title
+    this.description = description
+    this.category = category
+    this.location = location
+    this.createdAt = createdAt
+    this.cloneOf = cloneOf
+  }
+
+  public static create (owner: Visitor, title: string, description: string, category: Category, location: string, cloneOf: Claim) : Claim {
+    return new Claim(v4(), owner, title, description, category, location, new Date(), cloneOf)
+  }
+
+  public getId (): string {
+    return this.id
+  }
+
+  public getOwner (): Visitor {
+    return this.owner
+  }
+}
+
+export default Claim
