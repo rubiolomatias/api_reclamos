b5721a6e9062d564a90043d247f520b513ebb4ed
Matias Rubiolo <rubiolomatias2001@gmail.com>
2023-10-09 00:36:51 -0300

    feat:adeed repositories

---
+++ server/src/infrastructure/repositories/claim.repository.ts
@@ -0,0 +1,25 @@
+import Claim from '../../domain/entities/claim.entity'
+
+class ClaimRepository {
+  private claims: Claim[]
+
+  public constructor () {
+    this.claims = []
+  }
+
+  public async save (claim: Claim): Promise<void> {
+    const saveClaim = this.claims.find(a => a.getId() === claim.getId())
+    if (saveClaim) {
+      this.claims.splice(this.claims.indexOf(saveClaim), 1)
+    }
+    this.claims.push(claim)
+  }
+
+  public async findOneById (id: string): Promise<Claim | null> {
+    const claim = this.claims.find(a => a.getId() === id)
+
+    return claim || null
+  }
+}
+
+export default new ClaimRepository()
