06414825896d3d4998fe13ac46fd553ea2f9df2d
Matias Rubiolo <rubiolomatias2001@gmail.com>
2023-10-13 02:29:37 -0300

    feat:added createVisitorHandler

---
+++ server/src/application/handlers/create.visitor.handler.ts
@@ -0,0 +1,19 @@
+import Visitor from '../../domain/entities/visitor.entity'
+import visitorRepository, { VisitorRepository } from '../../infrastructure/repositories/visitor.repository'
+import CreateVisitorCommand from '../commands/create.visitor.command'
+
+class CreateVisitorHandler {
+  private visitorRepository: VisitorRepository
+
+  public constructor (visitorRepository: VisitorRepository) {
+    this.visitorRepository = visitorRepository
+  }
+
+  public async execute (command: CreateVisitorCommand): Promise<void> {
+    const visitor = Visitor.create(command.getIp(), command.getNickname(), command.getPin())
+
+    await this.visitorRepository.save(visitor)
+  }
+}
+
+export default new CreateVisitorHandler(visitorRepository)
